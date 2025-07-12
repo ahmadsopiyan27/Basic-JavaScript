c05d4e3d7cdd31c908a3321a055ab1c3c15d44f5
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-12 19:35:02 +0700

    update

--- variable.js
+++ variable.js
@@ -2,8 +2,13 @@
 
 
 // constan tidak bisa diubah
-const satu = 1
-satu = 2
-console.log(satu)
+const a = 1
+// satu = 2
+// console.log(satu)
 
 // var let -> bisa diubah
+let c = 2
+c = 3
+var b = 4
+var b = 45
+console.log(b)
